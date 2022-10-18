27ef6f77ebb79d5e152091f79b0dac6559fe0ee7
Dahi <abdelrahmanhishamdahi@gmail.com>
2022-10-19 00:34:48 +0200

    Update javascript.js

--- app/javascript.js
+++ app/javascript.js
@@ -1,7 +1,6 @@
 var imageswiper = new Swiper(".images-slider", {
   effect: "cards",
   grabCursor: true,
-  mousewheel: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -19,6 +18,7 @@ var infoswiper = new Swiper(".info-slider", {
     el: ".swiper-pagination-info",
     dynamicBullets: true,
   },
+
   allowTouchMove: false,
 });
 
@@ -36,27 +36,3 @@ document.addEventListener(
   },
   true
 );
-
-let menuIcon = document.querySelector(".menuIcon");
-let nav = document.querySelector(".overlay-menu");
-
-menuIcon.addEventListener("click", () => {
-  if (nav.style.transform != "translateX(0%)") {
-    nav.style.transform = "translateX(0%)";
-    nav.style.transition = "transform 0.2s ease-out";
-  } else {
-    nav.style.transform = "translateX(-100%)";
-    nav.style.transition = "transform 0.2s ease-out";
-  }
-});
-
-// Toggle Menu Icon ========================================
-let toggleIcon = document.querySelector(".menuIcon");
-
-toggleIcon.addEventListener("click", () => {
-  if (toggleIcon.className != "menuIcon toggle") {
-    toggleIcon.className += " toggle";
-  } else {
-    toggleIcon.className = "menuIcon";
-  }
-});
